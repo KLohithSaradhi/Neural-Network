97dc819d8cbe8cac5ee1746019ba51873093767b
Logith <lohith21110257@snuchennai.edu.in>
2022-11-19 21:30:04 +0530

    Update Main.py

--- SmallNetwork/Main.py
+++ SmallNetwork/Main.py
@@ -1,16 +1,18 @@
 from Network import *
 import pandas as pd
 
-data = pd.read_csv("data.csv")
-trainInputData = data.iloc[:700,:3]
-trainOutputData = data.iloc[:700,3:]
+data = pd.read_csv("normalizedData.csv")
+trainInputData = np.array(data.iloc[:20,1:4])
+trainOutputData = np.array(data.iloc[:20,4:])
 
 network = HardCodeNN(trainInputData, trainOutputData)
+
 print(network.layer1)
 print(network.layer2)
+print("=============")
 
-print(network.giveOutput(np.array([[40,30,20], [20,30,40]])))
+output = network.giveOutput()
 
+delta = network.backPropagate(output)
 
-
-
+network.updateWeights(delta)
